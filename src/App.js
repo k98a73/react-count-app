19a5b34fd9b6c8e76656ac36cb62afadb5595921
k98a73 <100816862+k98a73@users.noreply.github.com>
2022-05-25 11:03:50 +0900

    ボタン作成

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
-
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <Button btn_txt="クリックしてね" />
     </div>
   );
+}
+
+function Button(props) {
+  return (
+    <button>{props.btn_txt}</button>
+  )
 }
 
 export default App;
